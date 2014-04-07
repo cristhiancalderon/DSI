ad616813cf51dfd0b51c791123d8f8312c5edcd4
cristhiancalderon <cristhian.calderon.mackay@gmail.com>
2014-04-07 17:17:20 -0300

    nuevo cambio web

--- TallerGit/src/cl/taller/git/main.java
+++ TallerGit/src/cl/taller/git/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 	
 		System.out.println("segundo commit");
-			System.out.println("linea agregada desde la web");
+			System.out.println("linea agregada desde la web, modificado xD");
 
 	}
 
